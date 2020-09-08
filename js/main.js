4c3bd666d9c16b7930a9b79fa4b121fe65afda07
Inna Taranenko <inna.taranenko@gmail.com>
2020-09-08 19:20:23 +0300

    adaptive codes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,87 @@
+(function () {
+    const burger = document.querySelector(".menu__toggle");
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("menu__toggle-active");
+    });
+})();
 
+$(".menu__toggle").click(function (e) {
+    e.preventDefault();
+    $(".menu__list").toggleClass("menu__list__show");
+    $(".body").toggleClass("body__lock");
+});
 
 $(document).ready(function () {
 
-    $('.thanks__feedback').slick({
-        lazyLoad: 'ondemand',
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i> </button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>'
+    $(".thanks__feedback").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        responsive: [
+            {
+                breakpoint: 1150,
+                settings: {
+                    slidesToShow: 3,
+                    centerMode: true,
+                    centerPadding: "50px",
+                },
+            },
+            {
+                breakpoint: 950,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    slidesToShow: 2,
+                    centerPadding: "100px",
+                },
+            },
+            {
+                breakpoint: 780,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    slidesToShow: 2,
+                    centerPadding: "70px",
+                },
+            },
+            {
+                breakpoint: 705,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    slidesToShow: 2,
+                    centerPadding: "30px",
+                },
+            },
+            {
+                breakpoint: 620,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    slidesToShow: 2,
+                    centerPadding: "10px",
+                },
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    slidesToShow: 2,
+                    centerPadding: "0px",
+                },
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    slidesToShow: 1,
+                    centerPadding: "0px",
+                },
+            },
+        ],
     });
+
 
 })
